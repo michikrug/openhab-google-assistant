ee523f27b9f360a926507750789cfa3cededfab2
Michael Krug <michi.krug@gmail.com>
2021-05-21 22:06:44 +0200

    Add tests for doorbell
    Signed-off-by: Michael Krug <michi.krug@gmail.com>

---
+++ tests/devices/doorbell.test.js
@@ -0,0 +1,34 @@
+const Device = require('../../functions/devices/doorbell.js');
+
+describe('Doorbell Device', () => {
+  test('isCompatible', () => {
+    expect(
+      Device.isCompatible({
+        metadata: {
+          ga: {
+            value: 'DOORBELL'
+          }
+        }
+      })
+    ).toBe(true);
+  });
+
+  test('matchesItemType', () => {
+    expect(Device.matchesItemType({ type: 'String' })).toBe(false);
+    expect(Device.matchesItemType({ type: 'Switch' })).toBe(true);
+    expect(Device.matchesItemType({ type: 'Group', groupType: 'String' })).toBe(false);
+    expect(Device.matchesItemType({ type: 'Group', groupType: 'Switch' })).toBe(true);
+  });
+
+  test('getState', () => {
+    expect(Device.getState({})).toStrictEqual({});
+  });
+
+  test('getNotification', () => {
+    expect(Device.getNotification({})).toStrictEqual({});
+    expect(Device.getNotification({ state: 'ON' }).ObjectDetection).not.toBeUndefined();
+    expect(
+      Device.getNotification({ state: 'OFF', metadata: { ga: { config: { inverted: true } } } }).ObjectDetection
+    ).not.toBeUndefined();
+  });
+});
